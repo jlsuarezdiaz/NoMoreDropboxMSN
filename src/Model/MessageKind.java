78b40b895680f57d2d24a573c60acf1ccfa09c81
jlsuarezdiaz <jlsuarezdiaz@correo.ugr.es>
2016-07-27 17:26:19 +0200

    New message structure for communication client-server.

--- src/Model/MessageKind.java
+++ src/Model/MessageKind.java
@@ -1,9 +1,13 @@
 ////////////////////////////////////////////////////////////////////////////////
 // Author: Juan Luis Suarez Diaz
 // Jun, 2015
-// Dropbox MSN
+// No More Dropbox MSN
 ////////////////////////////////////////////////////////////////////////////////
 package Model;
+
+import static java.util.Arrays.stream;
+import java.util.Map;
+import static java.util.stream.Collectors.toMap;
 
 /**
  * MessageKind enum.
@@ -11,7 +15,79 @@ package Model;
  * @author Juan Luis
  */
 public enum MessageKind {
-    HELO,LOGIN,SEND,CHANGEPRIVATE,CHANGESELECT,CHANGESTATE,IMALIVE,OK,ERR,BYE,
-    RECEIVEMSG,RECEIVEUSR,LOGOUT,CONFIRMPRV,CONFIRMSLCT,CONFIRMSTATE,
-    DISC,KILL,VERSION,UPDATE,FILE,SENDFILE,NOP, WAIT;
+    
+    //1xx - Connection messages.
+    HELO               (100),
+    LOGIN              (101),
+    BYE                (102),
+    LOGOUT             (103),
+    DISC               (104),
+    KILL               (105),
+    UPDATE_DOWNLOAD    (106),
+    
+    //2xx - OK Messages
+    OK                 (200),
+    OK_SENT            (201),
+    OK_PRIV            (202),
+    OK_SLCT            (203),
+    OK_STATE           (204),
+    OK_FILE            (205),
+    OK_VERSION         (206),
+    
+    //3xx - Communication messages
+    USERS              (300),
+    SEND               (301),
+    FILE               (302),
+    IMALIVE            (303),
+    CHANGE_PRIV        (304),
+    CHANGE_SLCT        (305),
+    CHANGE_STATE       (306),
+    
+    //4xx - Error messages
+    ERR                (400),
+    ERR_BADREQUEST     (401),
+    ERR_USEROVERFLOW   (402),
+    ERR_FORBIDDEN      (403),
+    ERR_NOTFOUND       (404),
+    WARN_NOTUPATED     (405),
+    ERR_NEEDUPDATE     (406),
+    ERR_JARNOTFOUND    (407),
+    
+    
+    //0xx
+    NOP                (000)
+    ;
+    
+    //For mapping between enum and int
+    private final int msgCode;
+    
+    private final static Map<Integer, MessageKind> map =
+            stream(MessageKind.values()).collect(toMap(msg -> msg.msgCode, msg -> msg));
+    
+    /**
+     * Private constructor.
+     * @param msgCode Message code. 
+     */
+    private MessageKind(int msgCode){
+        this.msgCode=msgCode;
+    }
+    
+    /**
+     * Gets the MessageKind object associated to the given code.
+     * @param msgCode Message code.
+     * @return MessageKind whose code is \e msgCode.
+     */
+    public static MessageKind valueOf(int msgCode){
+        return map.get(msgCode);
+    }
+    
+    /**
+     * Gets the code of the message.
+     * @return Integer with the message code.
+     */
+    public int getMessageCode(){
+        return msgCode;
+    }
+    
+    
 }
